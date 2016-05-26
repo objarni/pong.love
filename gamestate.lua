a3f8525a0a5c857dd9e38de0b5a326e1a938b373
Olof Bjarnason <olof.bjarnason@gmail.com>
2016-05-26 22:31:04 +0200

    ball shader, score system, ball reset behaviour

--- gamestate.lua
+++ gamestate.lua
@@ -11,132 +11,170 @@ PAD_SPEED = 500
 PAD0IMAGE = love.graphics.newImage('pad0.png')
 PAD1IMAGE = love.graphics.newImage('pad1.png')
 BALLSIZE=20
-BALLSPEED=350
+BALLSPEED=250
 
 
 function build_game_state()
-	local pad = {}
+    local pad = {}
+    local lightup = love.graphics.newShader[[
+        extern vec2 centre;
+        vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+          //vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
+          float c = min(0.5, 1-distance(centre, screen_coords) / 400.0);
+          return vec4(c);
+        }
+      ]]
 
-	local keybindings = {}
-	keybindings[0] = { w='up', s='down' }
-	keybindings[1] = { up='up',down='down' }
+    local keybindings = {}
+    keybindings[0] = { w='up', s='down' }
+    keybindings[1] = { up='up',down='down' }
 
-	pad[0] = { name='pad0image',
-	           x=PAD_X_OFFSET,
-			   y=PAD_START_Y,
-			   color=PAD1_COLOR,
-			   score = 0,
-			   up = false,
-			   down = false,
-			   image = PAD0IMAGE,
-			   scoreX = SCREEN_WIDTH/8
-			 }
-	pad[1] = { name='pad1',
-	           x=SCREEN_WIDTH-PAD_W-PAD_X_OFFSET,
-	           y=PAD_START_Y,
-	           color=PAD2_COLOR,
-	           score = 0,
-			   up = false,
-			   down = false,
-			   image = PAD1IMAGE,
-			   scoreX = SCREEN_WIDTH/8*7
-	       	 }
-	assert(pad[0].image)
-	assert(pad[1].image)
+    function resetPad(pad)
+        pad.score = 0
+        y=PAD_START_Y
+    end
+    pad[0] = { name='pad0image',
+               x=PAD_X_OFFSET,
+               y=PAD_START_Y,
+               color=PAD1_COLOR,
+               up = false,
+               down = false,
+               image = PAD0IMAGE,
+               scoreX = SCREEN_WIDTH/8,
+               reset = resetPad
+             }
+    pad[1] = { name='pad1',
+               x=SCREEN_WIDTH-PAD_W-PAD_X_OFFSET,
+               color=PAD2_COLOR,
+               up = false,
+               down = false,
+               image = PAD1IMAGE,
+               scoreX = SCREEN_WIDTH/8*7,
+               reset = resetPad
+             }
+    for i = 0, 1 do
+        pad[i].reset(pad[1])
+    end
 
-	ball = {
-		reset = function(ball)
-			ball.x=SCREEN_WIDTH/2
-			ball.y=SCREEN_HEIGHT/2
-			ball.dx = -1
-			ball.dy = -1
-		end
-	}
-	ball.reset(ball)
+    assert(pad[0].image)
+    assert(pad[1].image)
 
-	function drawBall(ball)
-		love.graphics.circle('fill',
-			ball.x-BALLSIZE/2,
-			ball.y-BALLSIZE/2,
-			BALLSIZE)
-	end
+    local dx = 1
+    ball = {
+        reset = function(ball)
+            ball.x=SCREEN_WIDTH/2
+            ball.y=SCREEN_HEIGHT/2
+            if ball.dx then ball.dx = -ball.dx else ball.dx = -1 end
+            ball.dy = 1 - 2*math.random(0, 1)
+        end
+    }
+    ball.reset(ball)
 
-	function drawPad(pad)
-		love.graphics.setColor(pad.color)
-		love.graphics.rectangle('fill',
-			pad.x, pad.y,
-			PAD_W, PAD_H)
-		assert(pad.image)
-		love.graphics.draw(pad.image, pad.x, pad.y)
-	end
+    function drawBall(ball)
+        love.graphics.circle('fill',
+            ball.x,
+            ball.y,
+            BALLSIZE)
+        lightup:send('centre', {ball.x, SCREEN_HEIGHT-ball.y})
+        love.graphics.setBlendMode('additive')
+        love.graphics.setShader(lightup)
+        love.graphics.rectangle('fill', 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+        love.graphics.setShader()
+        love.graphics.setBlendMode('alpha')
+    end
 
-	function drawScore(pad)
-		love.graphics.setColor(pad.color)
-		love.graphics.printf(pad.score, pad.scoreX, 0, 50, 'center')
-	end
+    function drawPad(pad)
+        love.graphics.setColor(pad.color)
+        love.graphics.rectangle('fill',
+            pad.x, pad.y,
+            PAD_W, PAD_H)
+        assert(pad.image)
+        love.graphics.draw(pad.image, pad.x, pad.y)
+    end
 
-	function updatePadStateByKey(key, pressed, pad, bindings)
-		if bindings[key] then
-			pad[bindings[key]] = pressed
-		end
-	end
+    function drawScore(pad)
+        love.graphics.setColor(pad.color)
+        love.graphics.printf(pad.score, pad.scoreX, 0, 50, 'center')
+    end
 
-	function updatePad(pad, dt)
-		if pad.up then
-			-- debugtxt = 'moving up'
-			pad.y = pad.y - dt * PAD_SPEED
-		end
-		if pad.down then
-			-- debugtxt = 'moving down'
-			pad.y = pad.y + dt * PAD_SPEED
-		end
-		pad.y = crop(pad.y, 0, SCREEN_HEIGHT-PAD_H)
-	end
+    function updatePadStateByKey(key, pressed, pad, bindings)
+        if bindings[key] then
+            pad[bindings[key]] = pressed
+        end
+    end
 
-	function updateBall(ball, dt)
-		ball.x = ball.x + ball.dx * dt * BALLSPEED
-		ball.y = ball.y + ball.dy * dt * BALLSPEED
-		if ball.y < 0 or ball.y > SCREEN_HEIGHT then
-			ball.dy = -ball.dy
-		end
-		if ball.x < 0 then
-			ball.reset(ball)
-		end
-	end
+    function updatePad(pad, dt)
+        if pad.up then
+            -- debugtxt = 'moving up'
+            pad.y = pad.y - dt * PAD_SPEED
+        end
+        if pad.down then
+            -- debugtxt = 'moving down'
+            pad.y = pad.y + dt * PAD_SPEED
+        end
+        pad.y = crop(pad.y, 0, SCREEN_HEIGHT-PAD_H)
+    end
 
-	local state = lovestate()
+    function scoreTo(padNum)
+        local newScore = pad[padNum].score + 1
+        if newScore == 2 then
+            switch_state('start_state')
+        else
+            pad[padNum].score = newScore
+        end
+    end
 
-	state.init = function()
-		love.graphics.setBackgroundColor(  25, 174, 255 )
-	end
+    function updateBall(ball, dt)
+        ball.x = ball.x + ball.dx * dt * BALLSPEED
+        ball.y = ball.y + ball.dy * dt * BALLSPEED
+        if ball.y < 0 or ball.y > SCREEN_HEIGHT then
+            ball.dy = -ball.dy
+        end
+        if ball.x < 0 then
+            ball.reset(ball)
+            scoreTo(1)
+        end
+        if ball.x > SCREEN_WIDTH then
+            ball.reset(ball)
+            scoreTo(0)
+        end
+    end
 
-	state.draw = function()
-		for i = 0, 1 do
-			drawScore(pad[i])
-			drawPad(pad[i])
-		end
-		drawBall(ball)
-	end
+    local state = lovestate()
 
-	state.update = function( dt )
-		-- debugtxt = tostring(pad[0].up) .. ' ' .. tostring(pad[0]['down'])
-		for i = 0, 1 do
-			updatePad(pad[i], dt)
-		end
-		updateBall(ball, dt)
-	end
+    state.init = function()
+        pad[0].reset(pad[0])
+        pad[1].reset(pad[1])
+        love.graphics.setBackgroundColor(  25, 174, 255 )
+    end
 
-	state.keypressed = function ( key )
-		updatePadStateByKey(key, true, pad[0], keybindings[0])
-		updatePadStateByKey(key, true, pad[1], keybindings[1])
-	end
+    state.draw = function()
+        for i = 0, 1 do
+            drawScore(pad[i])
+            drawPad(pad[i])
+        end
+        drawBall(ball)
+    end
 
-	state.keyreleased = function ( key )
-		updatePadStateByKey(key, false, pad[0], keybindings[0])
-		updatePadStateByKey(key, false, pad[1], keybindings[1])
-	end
+    state.update = function( dt )
+        -- debugtxt = tostring(pad[0].up) .. ' ' .. tostring(pad[0]['down'])
+        for i = 0, 1 do
+            updatePad(pad[i], dt)
+        end
+        updateBall(ball, dt)
+    end
 
-	return state
+    state.keypressed = function ( key )
+        updatePadStateByKey(key, true, pad[0], keybindings[0])
+        updatePadStateByKey(key, true, pad[1], keybindings[1])
+    end
+
+    state.keyreleased = function ( key )
+        updatePadStateByKey(key, false, pad[0], keybindings[0])
+        updatePadStateByKey(key, false, pad[1], keybindings[1])
+    end
+
+    return state
 end
 
 
